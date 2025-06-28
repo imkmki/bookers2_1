14bb4b16fe778a383e66e9e0ef31543477d0dd4b
imkmki <209921994+imkmki@users.noreply.github.com>
2025-06-28 15:25:03 +0000

    画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
-
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+  
   def edit
     @user = User.find(params[:id])
   end
